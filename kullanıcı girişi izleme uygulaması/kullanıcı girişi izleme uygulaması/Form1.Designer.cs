9566cd0b1631a228f1360c3ae0b0137f9b7e4a99
kit-scorp <150590198+kit-scorp@users.noreply.github.com>
2023-11-13 19:06:39 +0300

    Add files via upload

---
+++ kullanıcı girişi izleme uygulaması/kullanıcı girişi izleme uygulaması/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace kullanıcı_girişi_izleme_uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(241, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(216, 33);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(241, 149);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(216, 33);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(364, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "log in";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(100, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "username :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(100, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(124, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 24);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ kullanıcı girişi izleme uygulaması/kullanıcı girişi izleme uygulaması/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace kullanıcı_girişi_izleme_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        private object smtp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+            string username = richTextBox1.Text;
+            string password = richTextBox2.Text;
+
+            if (username == "admin" && password == "1234")
+            {
+                label3.Text = "Giriş başarılı!";
+                button1.BackColor = Color.White;
+
+                try
+                {
+                    button1.BackColor = Color.White;
+                    MailMessage mail = new MailMessage();
+                    SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com");
+
+                    // SMTP kimlik doğrulama bilgileri
+                    smtp.Credentials = new System.Net.NetworkCredential("GÖNDEREN OUTLOOK ADRESİNİ GİRİNİZ", "HESAP ŞİFRESİ GİRİNİZ");
+
+
+                    mail.From = new MailAddress("GÖNDEREN OUTLOOK ADRESİNİ GİRİNİZ");
+                    mail.To.Add("GÖNDERİLECEK E POSTA ADRESİNİ GİRİNİZ");
+                    mail.Subject = "Hesaba giriş yapıldı";
+                    mail.Body = "Hesaba giriş yapıldı.";
+
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(mail);                   
+                    MessageBox.Show("E-posta gönderildi.");
+                }
+                catch (Exception ex)
+                {
+                    button1.BackColor = Color.White;
+                    MessageBox.Show("E-posta gönderme hatası: " + ex.Message);
+                }
+            }
+            else
+            {
+                
+                MessageBox.Show("Kullanıcı adı veya şifre yanlış!");
+                button1.BackColor = Color.White;
+            }
+        }
+    }
+}
